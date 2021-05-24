f3cf47d3268a7153cad8f724d36361bef037baac
Oren stern <36824291+orenStern2@users.noreply.github.com>
2021-05-24 10:02:55 +0300

    Update pagerduty.ps1

--- nbproject/pagerduty.ps1
+++ nbproject/pagerduty.ps1
@@ -1,9 +1,11 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Content-Type", "application/json")
-
-$body = "{`n    `"payload`": {`n        `"summary`": `"Testing from PowerShell`",`n        `"severity`": `"critical`",`n        `"component`": `"xyz`",`n        `"source`": `"PowerShell`",`n        `"group`": `"test`",`n        `"class`": `"alert`",`n        `"custom_details`": {`n            `"field1`": `"value1`"`n        }`n    },`n    `"routing_key`": `"R029PPL030SPPECQN32BD3A3N1WNMB42`",`n    `"dedup_key`": `"`",`n    `"event_action`": `"trigger`"`n}"
-
-$response = Invoke-RestMethod 'https://events.pagerduty.com/v2/enqueue' -Method 'POST' -Headers $headers -Body $body
-$response | ConvertTo-Json
-
+import re, time
+searchFor = input('What would you like to search for?:')
+browser = webdriver.Firefox()
+browser.get("https://google.com")
+searchElem = browser.find_element_by_css_selector('.gLFyf')
+searchElem.send_keys(searchFor)
+searchButton  = browser.find_element_by_css_selector('.FPdoLc > center:nth-child(1) > input:nth-child(1)')
+searchButton.click()
+searchText = browser.find_element_by_css_selector('html body#gsr.srp.tbo.vasq div#main div#cnt.big div.mw div#rcnt div.col div#center_col div#res.med div#search div div#rso div.g')
+print(searchText.text)
+browser.quit()
